9b091cd75bb3c8550b97fa7808e689e768c0f4aa
codephillip <codephillip@gmail.com>
2018-03-17 10:57:18 +0300

    reactivated pagination

--- busticket/settings.py
+++ busticket/settings.py
@@ -122,9 +122,9 @@ REST_FRAMEWORK = {
     # 'DEFAULT_PARSER_CLASSES': (
     #     'rest_framework.parsers.JSONParser',
     # ),
-   # 'DEFAULT_PAGINATION_CLASS':
-   #    'rest_framework.pagination.PageNumberPagination',
-   #     'PAGE_SIZE': 100
+   'DEFAULT_PAGINATION_CLASS':
+      'rest_framework.pagination.PageNumberPagination',
+       'PAGE_SIZE': 1000000
 }
 
 
