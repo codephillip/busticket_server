9fc1e7c869f2915788e5d93162c09eee9cbcddce
codephillip <codephillip@gmail.com>
2018-08-04 20:34:26 +0300

    refactored code

--- busticket/urls.py
+++ busticket/urls.py
@@ -7,12 +7,12 @@ from myapp.views import FeedbackView, BusCompanyView, BusView, OrderView, Locati
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index, name='index'),
-    url(r'^api/v1/routes$', RouteView.as_view(), name='routes_route'),
-    url(r'^api/v1/locations$', LocationView.as_view(), name='locations_route'),
-    url(r'^api/v1/orders$', OrderView.as_view(), name='orders_route'),
-    url(r'^api/v1/customers$', views.customers_route, name='customers_route'),
+    url(r'^api/v1/routes$', RouteView.as_view(), name='routes'),
+    url(r'^api/v1/locations$', LocationView.as_view(), name='locations'),
+    url(r'^api/v1/orders$', OrderView.as_view(), name='orders'),
+    url(r'^api/v1/customers$', views.customers_route, name='customers'),
     url(r'^api/v1/customers/(?P<pk>[-\w]+)/orders$', CustomerOrdersView.as_view(), name='customers_orders'),
-    url(r'^api/v1/buses$', BusView.as_view(), name='buses_route'),
-    url(r'^api/v1/bus_companys$', BusCompanyView.as_view(), name='bus_companys_route'),
+    url(r'^api/v1/buses$', BusView.as_view(), name='buses'),
+    url(r'^api/v1/bus_companys$', BusCompanyView.as_view(), name='bus_companys'),
     url(r'^api/v1/feedbacks$', FeedbackView.as_view(), name='feedback'),
 ]
